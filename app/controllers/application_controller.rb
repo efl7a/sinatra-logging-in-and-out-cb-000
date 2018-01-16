48119af8298ca68668aa3ad5de71da95aacf34cb
Heather Christman <efl7a@yahoo.de>
2018-01-16 18:07:05 -0500

    not complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,38 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    binding.pry
+    if User.find_by(username: params[:username], password: params[:password])
+      current_user = User.find_by(username: params[:username], password: params[:password])
+      session[:user_id] = current_user.id
+      redirect '/account'
+    else
+      redirect '/error'
+    end
   end
 
   get '/account' do
+    if Helpers.is_logged_in?
+      @current_user = Helpers.current_user
+      erb :account
+    else
+      redirect '/error'
+    end
 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
+  get '/error' do
+    erb :error
+  end
+
+  post '/logout' do
+    session.clear
+    redirect '/logout'
+  end
 
 end
-
